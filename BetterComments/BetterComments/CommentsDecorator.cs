f3c3f40b45eff01b64be4dfa19e11abbaeb70e98
Omar Rwemi <omsharp@gmail.com>
2016-04-12 20:37:43 +0200

    Italicize comments
    Italicizing comments is working properly. Still need to find the best
    text properties to set.

---
+++ BetterComments/BetterComments/CommentsDecorator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace BetterComments.BetterComments
+{
+    internal sealed class CommentsDecorator
+    {
+        private bool formattingComments;
+        private readonly IWpfTextView textView;
+        private readonly IClassificationFormatMap formatMap;
+        private readonly IClassificationType textClassification;
+        private readonly IClassificationTypeRegistryService typeRegistryService;
+
+        private static readonly List<string> commentTypes = new List<string>() { "comment", "xml doc comment", "vb xml doc comment", "xml comment", "html comment", "xaml comment" };
+        private static readonly List<string> docTagTypes = new List<string>() { "xml doc tag", "vb xml doc tag", "xml doc attribute" };
+
+        public CommentsDecorator(IWpfTextView textView,
+            IClassificationFormatMap formatMap,
+            IClassificationTypeRegistryService typeRegistryService)
+        {
+            this.textView = textView;
+            this.formatMap = formatMap;
+            this.typeRegistryService = typeRegistryService;
+            this.textClassification = this.typeRegistryService.GetClassificationType("text");
+
+            FormatComments();
+
+            // fires when the text editor open. Need it only for the first time.
+            this.textView.GotAggregateFocus += TextView_GotAggregateFocus;
+
+            // this.textView.Closed += TextView_Closed;
+        }
+
+        private void TextView_GotAggregateFocus(object sender, EventArgs e)
+        {
+            var view = sender as ITextView;
+            if (view != null)
+                view.GotAggregateFocus -= TextView_GotAggregateFocus;
+
+            Debug.Assert(!formattingComments, "How can we be updating *while* the view is getting focus?");
+
+            FormatComments();
+        }
+
+        private void FormatComments()
+        {
+            try
+            {
+                formattingComments = true;
+
+                foreach (var type in commentTypes.Select(type => typeRegistryService.GetClassificationType(type)).Where(type => type != null))
+                {
+                    Italicize(type);
+                    Colorize(type);
+                }
+
+                foreach (var type in formatMap.CurrentPriorityOrder.Where(type => type != null))
+                {
+                    var name = type.Classification.ToLowerInvariant();
+                    if (!commentTypes.Contains(name) && name.Contains("comment"))
+                    {
+                        Italicize(type);
+                        Colorize(type);
+                    }
+                }
+            }
+            finally
+            {
+                formattingComments = false;
+            }
+        }
+
+        private void Colorize(IClassificationType type)
+        {
+            var properties = formatMap.GetTextProperties(type);
+
+            properties = properties.SetForeground(Color.FromRgb(65, 100, 80));
+
+            formatMap.SetTextProperties(type, properties);
+        }
+
+        private void Italicize(IClassificationType type)
+        {
+            var properties = formatMap.GetTextProperties(type);
+            var typeface = properties.Typeface;
+
+            if (typeface.Style == FontStyles.Italic)
+                return;
+            
+            var newTypeface = new Typeface(new FontFamily("Comic Sans MS"), FontStyles.Italic, FontWeights.Light, typeface.Stretch);
+            var newProps = properties.SetTypeface(newTypeface).SetFontRenderingEmSize(properties.FontRenderingEmSize + 1);
+
+            formatMap.SetTextProperties(type, newProps);
+        }
+    }
+}
---
+++ BetterComments/BetterComments/ViewCreationListener.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Win32;
+
+namespace BetterComments.BetterComments
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("code")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class ViewCreationListener : IWpfTextViewCreationListener
+    {
+       // private static readonly bool isEnabled;
+
+        static ViewCreationListener()
+        {
+           // isEnabled = IsEnabled();
+        }
+        
+#pragma warning disable 0649
+        [Import]
+        private IClassificationFormatMapService formatMapService;
+        [Import]
+        private IClassificationTypeRegistryService typeRegistry;
+#pragma warning restore 0649
+
+         public void TextViewCreated(IWpfTextView textView)
+        {
+            //if (isEnabled) //todo: check the registry thing
+                textView.Properties.GetOrCreateSingletonProperty(() =>
+                new CommentsDecorator(textView,
+                formatMapService.GetClassificationFormatMap(textView),
+                typeRegistry));
+        }
+
+        private static bool IsEnabled()
+        {
+            try
+            {
+                using (var subKey = Registry.CurrentUser.OpenSubKey(@"Software\BetterComments", false))
+                    return Convert.ToInt32(subKey?.GetValue("EnablItalics", 1)) != 0;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"BetterComments failed to read the registry: {ex.Message}");
+            }
+
+            return false;
+        }
+    }
+}
