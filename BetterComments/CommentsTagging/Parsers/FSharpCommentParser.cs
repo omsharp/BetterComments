3e4162353ab399210f771cf330c2270a8b6e7134
Omar Rwemi <omsharp@gmail.com>
2017-06-11 23:37:14 +0200

    Ignore multiline comments in F#

--- BetterComments/CommentsTagging/Parsers/FSharpCommentParser.cs
+++ BetterComments/CommentsTagging/Parsers/FSharpCommentParser.cs
@@ -45,54 +45,7 @@ namespace BetterComments.CommentsTagging
 
             commentSpans.Add(new SnapshotSpan(span.Snapshot, span.Start + startOffset, spanLength));
          }
-         else //! Comment spans multiple lines
-         {
-            var currentLine = span.Snapshot.GetLineFromPosition(span.Start);
-            var lineText = currentLine.GetText();
-
-            var startOffset = 0;
-            var spanLength = 0;
-
-            //! Handle first line, ONLY if it is more than just a comment starter
-            var trimmedLineText = lineText.Trim();
-            if ((trimmedLineText.Length - trimmedLineText.IndexOf("(*")) > 3)
-            {
-               startOffset = commentType == CommentType.Task
-                                             ? lineText.IndexOf("todo")
-                                             : lineText.IndexOfFirstChar(lineText.IndexOf("(*") + 3);
-
-               commentSpans.Add(new SnapshotSpan(span.Snapshot, currentLine.Start + startOffset, currentLine.Length - startOffset));
-            }
-
-            //! Handle the rest of the lines
-            var done = false;
-            while (!done)
-            {
-               currentLine = span.Snapshot.GetLineFromLineNumber(currentLine.LineNumber + 1);
-               lineText = currentLine.GetText();
-
-               if (!lineText.Contains("*)")) //! Line in the middle
-               {
-                  startOffset = lineText.IndexOfFirstChar();
-                  spanLength = currentLine.Length - startOffset;
-                  commentSpans.Add(new SnapshotSpan(span.Snapshot, currentLine.Start + startOffset, spanLength));
-               }
-               else //! The last line
-               {
-                  //! Handle it ONLY if it is more than just a comment ender.
-                  if (!lineText.Trim().StartsWith("*)"))
-                  {
-                     startOffset = lineText.IndexOfFirstChar();
-                     spanLength = lineText.IndexOfFirstCharReverse(lineText.IndexOf("*)") - 1) - startOffset + 1;
-                     commentSpans.Add(new SnapshotSpan(span.Snapshot, currentLine.Start + startOffset, spanLength));
-                  }
-
-                  done = true;
-               }
-            }
-
-         }
-
+        
          return new Comment(commentSpans, commentType);
       }
 
