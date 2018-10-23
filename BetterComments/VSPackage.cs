9715890a994523da18c947410ade687e0195ccce
Omar Rwemi <omsharp@gmail.com>
2018-10-23 20:12:46 +0200

    Derive VsPackage from AsyncPackage

--- BetterComments/VSPackage.cs
+++ BetterComments/VSPackage.cs
@@ -7,12 +7,12 @@ namespace BetterComments
 {
    [ProvideOptionPage(typeof(OptionsGeneralPage), "Better Comments", "General", 0, 0, true)]
    [ProvideOptionPage(typeof(OptionsTokensPage), "Better Comments", "Tokens", 0, 0, true)]
-   [PackageRegistration(UseManagedResourcesOnly = true)]
+   [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading =true)]
    [InstalledProductRegistration("#110", "#112", Vsix.Id, IconResourceID = 400)]
    [Guid(PACKAGE_GUID_STRING)]
    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly",
                      Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-   public sealed class VsPackage : Package
+   public sealed class VsPackage : AsyncPackage
    {
       public const string PACKAGE_GUID_STRING = "09e59564-c21a-44f8-ae2b-c2bc17facd07";
    }
